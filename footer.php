1bc19ecaf0d2427760f1b4d11bcc7e1731fbe0eb
amandabosso <amanda.kethilen@hotmail.com>
2024-11-21 17:25:30 -0300

    BD
    banco de dados

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer>
+        <h4> Desenvolvido por <a href="Mailto.amanda.kethilen@hotmail.com">Amanda Bosso </a></h4>
+    </footer>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<header>
+    <h1> Sistema de cabelereiro 🧏‍♂️</h1>
+    <nav>
+        <a href="servicos-listar.php"> Serviços</a>
+        <a href="clientes-listar.php"> Clientes</a>
+        <a href="agenda-listar.php"> Agenda</a>
+        <a href="produtos-listar.php">Produtos</a>
+        <a href="funcionarios-listar.php"> Funcionários</a>
+        <a href="fornecedores-listar.php"> Fornecedores</a>
+        <a href="suporte.php"> Suporte </a>
+    </nav>
+    </header>
